75582be298fab51426d03e2457048e4e4c86ed0d
AKhrianin <andrey.orlinsky@gmail.com>
2022-08-11 13:16:36 +0300

    Add new abstract class for all animals

---
+++ src/ua/com/javarush/akhrianin/natureGame/Main.java
@@ -0,0 +1,4 @@
+package ua.com.javarush.akhrianin.natureGame;
+
+public class Main {
+}
---
+++ src/ua/com/javarush/akhrianin/natureGame/npc/Animal.java
@@ -0,0 +1,8 @@
+package ua.com.javarush.akhrianin.natureGame.npc;
+
+public abstract class Animal{
+    abstract void eat();
+    abstract void move();
+    abstract void multiply();
+    abstract void die();
+}
